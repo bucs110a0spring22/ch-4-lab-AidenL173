1a575b0ab5327f869c020d806c77d1913f4e171d
Aiden Lin <98539623+AidenL173@users.noreply.github.com>
2022-03-06 22:05:56 +0000

    Completion, probably...

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+ < Using the turtle module, it draws sin, cosine and tangent curves. It also sets up the world coordinates and draws both axis to make the curves look like what is expected.>
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: < 5 >
 
-Grace days remaining: < # >/5
+Grace days remaining: < 5 >/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+ < None >
 
 ## REFERENCES:
- < List any outside resources used >
+ < https://docs.python.org/3.3/library/turtle.html?highlight=turtle#turtle.goto >
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+ < N/A >
--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 import turtle
+import math
 
 ########### Your Code here ##############
 # You should only have functions here
 # If you have anything outside of a function, 
 # then you do not fully understand functions
 # and should review how they work or ask for help
+def drawSineCurve(myturtle=None):
+  myturtle.up()
+  for i in range(-360,361):
+    y = math.sin(math.radians(i))
+    myturtle.goto(i,y)
+    myturtle.down()
 
+def setupWindow(mywindow=None):
+  turtle.setworldcoordinates(-360,-1,360,1)
+  turtle.bgcolor("white")
 
+def setupAxis(myturtle=None):
+  myturtle.up()
+  myturtle.goto(0,-1)
+  myturtle.down()
+  myturtle.goto(0,1)
+  myturtle.up()
+  myturtle.goto(360,0)
+  myturtle.down()
+  myturtle.goto(-360,0)
 
+def drawCosineCurve(myturtle=None):
+  myturtle.up()
+  for i in range(-360,361):
+    y = math.cos(math.radians(i))
+    myturtle.goto(i,y)
+    myturtle.down()
 
-
-
-
+def drawTangentCurve(myturtle=None):
+  myturtle.up()
+  for i in range(-360,361):
+    y = math.tan(math.radians(i))
+    myturtle.goto(i,y)
+    myturtle.down()
 
 ##########  Do Not Alter Any Code Past Here ########
 def main():
